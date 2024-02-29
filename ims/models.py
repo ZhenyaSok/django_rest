76f8a73fa4d8bf7c5e4fb2de23de77953458198b
kapit <kapitan.kub@gmail.com>
2024-02-29 14:27:58 +0500

    24.1

--- ims/models.py
+++ ims/models.py
@@ -34,7 +34,7 @@ class Subject(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='администратор')
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f'{self.title} {self.description}'
 
     class Meta:
         verbose_name = 'урок'
