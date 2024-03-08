c33cf29995e0c16f2fcfe7b8798396248bb870d8
kapit <kapitan.kub@gmail.com>
2024-03-08 18:09:29 +0500

    Добавлена пагинация на весь проект

--- config/settings.py
+++ config/settings.py
@@ -150,4 +150,11 @@ EMAIL_USE_SSL = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# django-rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3
+}
+
 
