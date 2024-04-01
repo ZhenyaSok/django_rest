cbd4ca821b10bfb53eeca7ff07de1a58c02d144b
kapit <kapitan.kub@gmail.com>
2024-04-01 16:25:06 +0500

    26.1

--- users/views.py
+++ users/views.py
@@ -3,12 +3,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, serializers
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
 from config import settings
 from users.models import User, Payment
 import stripe
 from lms.permissions import IsOwner, IsModerator
 from users.serializers import PaymentSerializer, UserSerializer
-from users.services import create_stripe_price, create_stripe_session
+from users.services import create_stripe_price, create_stripe_session, get_status_payment
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -21,7 +23,6 @@ class PaymentListApiView(generics.ListAPIView):
     # permission_classes = [IsAuthenticated]
 
 
-
 class PaymentCreateAPIView(generics.CreateAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
@@ -29,7 +30,6 @@ class PaymentCreateAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         course = serializer.validated_data.get('paid_course')
-        print(course)
 
         if not course:
             raise serializers.ValidationError('Укажите курс')
@@ -39,28 +39,11 @@ class PaymentCreateAPIView(generics.CreateAPIView):
             raise serializers.ValidationError('Укажите верную цену, должна совпадать с ценой на сайте')
 
         stripe_price_id = create_stripe_price(payment)
-        payment.payment_link = create_stripe_session(stripe_price_id)
+        payment.payment_link, payment.payment_id = create_stripe_session(stripe_price_id)
+
+        get_status_payment(payment.payment_id) # информация о статусе платежа, в дальнейшем можно реализовать ее отправку на почту
 
         payment.save()
-
-
-
-#     def post(self, request, *args, **kwargs):
-#         super(PaymentCreateAPIView, self).post(request, *args, **kwargs)
-#         checkout_session = stripe.checkout.Session.create(
-#             line_items=[
-#                 {
-#                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-#                     'price': 'price_1OzDyORoEJdjRqgtewTvrPqW',
-#                     'quantity': 1,
-#                 },
-#             ],
-#             mode='payment',
-#             success_url=settings.DOMAIN_NAME,
-#             cancel_url='http://127.0.0.1:8000/payment/'
-# ,
-#         )
-#         return HTTPResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
 
 
 class UserListAPIView(generics.ListAPIView):
